1536ee446040d241b950adab5e8fd4a2ab8cb432
TsheringYangden99 <07230084.sherubtse@rub.edu.bt>
2025-06-03 17:39:21 +0530

    Echoes of Time

--- app.js
+++ app.js
@@ -63,5 +63,7 @@ createTestimonialsTable();
 
 // Server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  //console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
+
 });
