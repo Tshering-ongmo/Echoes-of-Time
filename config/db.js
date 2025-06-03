0877c180fe7a45710ba02eca65d5106b22cf85da
TsheringYangden99 <07230084.sherubtse@rub.edu.bt>
2025-06-03 17:24:58 +0530

    Echoes of Time

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const db = pgp({
-host: process.env.DB_HOST || 'localhost', 
-port: 1234, database: process.env.DB_NAME || 'echoes-of_time', 
-user: process.env.DB_USER || 'postgres', 
-password: process.env.DB_PASS || 'bhutanese',
-ssl: {
-  rejectUnauthorized: false
-}
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = db;
